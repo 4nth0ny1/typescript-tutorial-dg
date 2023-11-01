d7997c195e154bc4dae30aeaf3f0be8443605e6d
4nth0ny1 <acatullo4@gmail.com>
2023-11-01 18:35:07 -0500

    added lesson-2

---
+++ lesson-2/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName;
+let meaningOfLife;
+let isLoading;
+let album; // union type, either is ok
+myName = '42';
+meaningOfLife = 24;
+isLoading = false;
+album = '4';
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ lesson-2/src/main.ts
@@ -0,0 +1,18 @@
+let myName: string
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: string | number; // union type, either is ok
+
+myName = '42'
+meaningOfLife = 24
+isLoading = false
+album = '4'
+
+const sum = (a:string, b:boolean) => {
+    return a + b
+}
+
+let postId: string | number 
+let isActive: number | boolean | string 
+
+let re: RegExp = /\w+/g
